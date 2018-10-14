e6b1e10441e450e028b5fdeeab94f898f34b24c2
wanderling <poobesh.g@gmail.com>
2018-10-14 19:32:32 +0530

    init

---
+++ keys.py
@@ -0,0 +1,1 @@
+azureKey="b2101b2ed9744c51b7dd0c0e7ecad979"
---
+++ main.py
@@ -0,0 +1,15 @@
+from vangogh import app
+import os
+import logging
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
